808c6c6d167e4aa5255f630df7190508fc8ce5cb
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-17 16:11:47 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Provider } from './context';
 import Card from './components/cards/card';
+import Spinner from './components/spinner';
 
 function App() {
   return (
     <Provider>
       <React.Fragment>
         <h1>Hello world</h1>
+        <Spinner />
         <Card/>
       </React.Fragment>
     </Provider>
--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
+import Spinner from '../spinner';
 
 class Card extends Component {
     render() {
         return (
             <Consumer>
                 {value => {
-                    console.log('in cards-->',value);
+                    const { confirmed, recovered, deaths, lastUpdate } = value;
+                    console.log(value);
+                    if( confirmed === undefined || confirmed.length == 0){
+                        return <Spinner/>
+                    }
+                    else{
+                        
+                    }
                 }}
             </Consumer>
         )
